b474b83df303a6f31096a2e9c5dc39341ab37a78
Filip Biedrzycki <filip.biedrzycki@ubikcomputing.com>
2016-05-12 19:58:58 +0200

    Exercise 6

--- main.c
+++ main.c
@@ -2,7 +2,21 @@
 
 int main(int argc,char *argv[])
 {
-int distance = 100;
-printf("You are %d miles away.\n",distance);
+  int distance = 100;
+  float power = 2.345f;
+  double super_power = 56789.5432;
+  char initial = 'A';
+  char first_name[]="Zed";
+  char empty[]="";
+  char last_name[]="Shaw";
+  printf("You are %d miles away.\n",distance);
+  printf("You have %f levels of power.\n",power);
+  printf("You have %f awesome super powers.\n",super_power);
+  printf("I have an initial %c.\n",initial);
+  printf("I have a first name %s.\n", first_name);
+  printf("I have a last name %s.\n", last_name);
+  printf("Empty: %s.\n", empty);
+  printf("My whole name is %s %c. %s. \n", first_name, initial, last_name);
+
   return 0;
 }
