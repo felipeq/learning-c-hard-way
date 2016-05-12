252b2ad8516166837355d84a3db96cb822311f03
Filip Biedrzycki <filip.biedrzycki@ubikcomputing.com>
2016-05-12 19:41:07 +0200

    Exercise 3

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main(int argc,char *argv[])
 {
-  puts("Hello world!");
+  int age= 10;
+  int height = 72;
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+
   return 0;
 }
